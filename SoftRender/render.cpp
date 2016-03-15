13cc8a9b94d8d1f963e2c8754e38e32e507eb6ac
silence394 <silence394@126.com>
2016-03-15 18:39:59 +0800

    first render

--- SoftRender/render.cpp
+++ SoftRender/render.cpp
@@ -13,15 +13,35 @@
 
 HWND	gHwnd;
 HDC		gWindowDc;
-char*	gFrameBuffer;
+HBITMAP	gHBitMap;
+HBITMAP	gOldBitMap;
+unsigned char*	gScreenBuffer;
 long	gScreenPitch;
 
+unsigned int** gFrameBuffer;
+
+int gWidth, gHeight = 0;
+long gLPitch = 0;
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
-bool WindowInit( void* param = NULL );
+bool WindowInit( int w, int h );
+int GameMain();
 
 int main()
 {
-	WindowInit();
+	WindowInit(800, 600);
+
+	//gScreenBuffer
+	//Init gFramebuffer.
+	{
+		char* ptr = (char*) malloc(gWidth * gHeight * 4);
+		gFrameBuffer = (unsigned int**) ptr;
+		for (int y = 0; y < gHeight; y ++)
+		{
+			gFrameBuffer[y] = (unsigned int*) (gScreenBuffer + gWidth * y * 4);
+		}
+		
+	}
 
 	MSG msg;
 
@@ -35,6 +55,7 @@ int main()
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
+		GameMain();
 	}
 
 	return 0;
@@ -61,12 +82,12 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
 
-bool WindowInit( void* param /* = NULL */ )
+bool WindowInit(int w, int h)
 {
 	WNDCLASS winclass;
 	HDC hdc;
+	LPVOID ptr;
 	RECT rect = {0, 0, 800, 600};
-
 
 	winclass.style			= CS_BYTEALIGNCLIENT;
 	winclass.lpfnWndProc	= WindowProc;
@@ -78,6 +99,20 @@ bool WindowInit( void* param /* = NULL */ )
 	winclass.hbrBackground	= (HBRUSH) GetStockObject(BLACK_BRUSH);
 	winclass.lpszMenuName	= NULL;
 	winclass.lpszClassName	= WINDOW_CLASS_NAME;
+
+	BITMAPINFO bitinfo;
+
+	bitinfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	bitinfo.bmiHeader.biWidth = w;
+	bitinfo.bmiHeader.biHeight = -h;
+	bitinfo.bmiHeader.biPlanes = 1;
+	bitinfo.bmiHeader.biBitCount = 32;
+	bitinfo.bmiHeader.biCompression = BI_RGB;
+	bitinfo.bmiHeader.biSizeImage = w * h * 4;
+	bitinfo.bmiHeader.biXPelsPerMeter = 0;
+	bitinfo.bmiHeader.biYPelsPerMeter = 0;
+	bitinfo.bmiHeader.biClrUsed = 0;
+	bitinfo.bmiHeader.biClrImportant = 0;
 
 	if (!RegisterClass(&winclass))
 		return false;
@@ -91,6 +126,17 @@ bool WindowInit( void* param /* = NULL */ )
 	hdc = GetDC(gHwnd);
 	gWindowDc = CreateCompatibleDC(hdc);
 	ReleaseDC(gHwnd, hdc);
+
+	gHBitMap = CreateDIBSection(gWindowDc, &bitinfo, DIB_RGB_COLORS, &ptr, 0, 0);
+	if (gHBitMap == NULL)
+		return false;
+
+	gOldBitMap = (HBITMAP) SelectObject(gWindowDc, gHBitMap);
+
+	gScreenBuffer = (unsigned char*) ptr;
+	gWidth = w;
+	gHeight = h;
+	gLPitch = w * 4;
 
 	AdjustWindowRect(&rect, GetWindowLong(gHwnd, GWL_STYLE), 0 );
 
@@ -107,6 +153,39 @@ bool WindowInit( void* param /* = NULL */ )
 	SetWindowPos(gHwnd, NULL, cx, cy, x1, y1, (SWP_NOCOPYBITS | SWP_NOZORDER | SWP_SHOWWINDOW));
 
 	ShowWindow( gHwnd, SW_NORMAL );
+
+	//Ã¿ÏñËØ4¸ö×Ö½Ú
+	memset(gScreenBuffer, 0, w * h * 4);
 	
 	return true;
+}
+
+int GameMain()
+{
+	//Clear framebuffer.
+	for (int y = 0; y < gHeight; y ++)
+	{
+		for ( int x = 0; x < gWidth; x ++ )
+		{
+			gFrameBuffer[y][x] = 0xff000000;
+		}
+	}
+
+	for (int i = 0; i < 1000; i ++)
+	{
+		int xx = rand() % (gWidth - 1);
+		int yy = rand() % (gHeight - 1);
+
+		int r = rand()%255;
+		int g = rand()%255;
+		int b = rand()%255;
+		gFrameBuffer[yy][xx] = 0xff<< 24 + r << 16 + g << 8 + b;
+	}
+	
+	HDC hdc = GetDC(gHwnd);
+	BitBlt( hdc, 0, 0, gWidth, gHeight, gWindowDc, 0, 0, SRCCOPY);
+	ReleaseDC( gHwnd, hdc );
+	
+	Sleep(30);
+	return 0;
 }
